83a851cae26b85e00662ac6abdb85871cdf29afa
braga-gustavo <gustavo.b.moraes2@gmail.com>
2023-10-13 18:13:11 -0300

    finalizing content

--- src/app/components/card/card-pricing/card-pricing.component.ts
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-card-pricing',
-  templateUrl: './card-pricing.component.html',
-  styleUrls: ['./card-pricing.component.css']
+	selector: 'app-card-pricing',
+	templateUrl: './card-pricing.component.html',
+	styleUrls: ['./card-pricing.component.css'],
 })
 export class CardPricingComponent implements OnInit {
+	@Input()
+	gamePrice: string = '';
+	@Input()
+	gameType: string = '';
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+	selector: 'app-card',
+	templateUrl: './card.component.html',
+	styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+	@Input()
+	gamePicture: string = "";
+	@Input()
+	gameLabel: string = "";
+	@Input()
+	gamePrice: string = "";
+	@Input()
+	gameType: string = "";
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
